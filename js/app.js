8b71afc19d050d040400b6043072cf02bb989079
Gyan Ganga Public School <104884742+gyangangaschool@users.noreply.github.com>
2022-07-19 09:08:22 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+const image = document.getElementById('file');
+document.getElementById("sub").addEventListener("click", function () {
+    clear();
 
+    const find = document.querySelector('#search').value;
+    const link = document.querySelector('#link');
+    
+    if (find === search.value) {
+
+        var y = link.href = "./halltickets/" + find + ".pdf";
+        const img = document.createElement("pdf");
+        const t = img.src = "./halltickets/" + find + ".pdf";
+
+        image.appendChild(img)
+        link.textContent = y;
+
+    }
+    if (find =! search.value) {
+        link.textContent = ("Invalid Response");
+    }
+
+    function clear() {
+        while (image.firstChild)
+            image.removeChild(image.firstChild)
+    }
+});
